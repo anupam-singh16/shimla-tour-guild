831380b5c06a834d30ecb1a5fc6f96c852f7df53
Anupam Singh <anupamsingh16june@gmail.com>
2026-01-15 13:21:59 +0530

    added new icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="sticky top-0 bg-white shadow z-50">
       <div className="max-w-6xl mx-auto p-4 flex justify-between">
-        <h1 className="text-xl font-bold">🏔️ Shimla Tour Guide</h1>
+        <h1 className="text-xl font-bold">🏔️ Shimla Tour Guide by Anupam Singh</h1>
       </div>
     </nav>
   );
